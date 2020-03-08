85f72c75530063edc7edcfd59000b1fc7f6e6caf
melvintzw <melvintan123@gmail.com>
2020-03-09 02:39:15 +0800

    modified bot to take in labeled datetime intervals and return labeled overlaps.

--- bot.py
+++ bot.py
@@ -3,13 +3,18 @@
 
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
-
+import overlap_finder
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
-        await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
+        
+        dt_list = overlap_finder.parse_dt_string(turn_context.activity.text)
+        overlap_dict = overlap_finder.find_all_common_intervals(dt_list)
+        to_print = overlap_finder.format_overlaps(overlap_dict)
+
+        await turn_context.send_activity(f"{to_print}")
 
     async def on_members_added_activity(
         self,
