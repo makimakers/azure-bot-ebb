ba215462e6e613c169a30326a68e652630172d76
melvintzw <melvintan123@gmail.com>
2020-04-03 18:34:16 +0800

    Removed redundant function and function call. Improved docstrings according to PEP287 standard. Removed test cases from module.

--- overlap_finder.py
+++ overlap_finder.py
@@ -5,7 +5,6 @@ from datetime import timedelta
 from intervaltree import Interval, IntervalTree
 from warnings import warn
 
-# TODO: move test cases to another module.
 
 FORMAT_MSG = ("Expected format : '<NAME>: <DATE> <TIME> <INTERVAL END>;'.\n\n"
               "For TIME, the hours and mins MUST be separated by ':' or time will "
@@ -16,74 +15,6 @@ EXAMPLE_MSG = ("Bob: 02 feb 10:00 + 2h15m;\n\n"
                "Bob: 02 feb 13:00-16:30;\n\n"
                "Joe: 2 feb 2:00pm-4:00pm;\n\n"
                "Sally: 02 feb 3:00p - 03 feb 1:00a")
-
-# one interval
-tc_0 = [Interval(1, 3, "aaron")]
-
-# no overlap
-tc_1 = [Interval(1, 2, "aaron"), Interval(3, 4, "ben"), Interval(5, 6, "charlotte")]
-
-# separate overlaps
-tc_2 = [Interval(1, 3, "aaron"), Interval(2, 4, "ben"), Interval(3, 5, "charlotte")]
-
-# shared overlap
-tc_3 = [Interval(1, 3, "aaron"), Interval(2, 4, "ben"), Interval(2, 5, "charlotte")]
-
-# 4 intervals
-tc_4 = [Interval(1, 3, "aaron"), Interval(2, 4, "ben"),
-        Interval(3, 5, "charlotte"), Interval(1, 6, "dan")]
-
-# more test cases
-#----------------
-
-# input: no overlap
-# expected: no common interval
-tc_5 = [
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=2, hour=0), "user_1"),
-    Interval(dt(year=2018, month=1, day=2, hour=0), dt(year=2018, month=1, day=3, hour=0), "user_2"),
-    Interval(dt(year=2018, month=1, day=3, hour=0), dt(year=2018, month=1, day=4, hour=0), "user_3")
-]
-
-# input: two overlap
-# expected: common interval at 2018, 1, 2, 0 - 2018, 1, 3, 0, for all users
-# common interval at 2018, 1, 2, 0 - 2018, 1, 4, 0, for user_1 and user_3.
-tc_6 = [
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=4, hour=0), "user_1"),
-    Interval(dt(year=2018, month=1, day=2, hour=0), dt(year=2018, month=1, day=3, hour=0), "user_2"),
-    Interval(dt(year=2018, month=1, day=2, hour=0), dt(year=2018, month=1, day=5, hour=0), "user_3")
-]
-
-# input: complete overlap
-# expected: one common interval at 2018, 1, 1, 0 - 2018, 1, 4, 0, for all users
-tc_7 = [
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=4, hour=0), "user_1"),
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=4, hour=0), "user_2"),
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=4, hour=0), "user_3")
-]
-
-# input: user 1 has 2 different intervals
-# expected: two common intervals at [2018, 1, 1, 0 - 2018, 1, 2, 0] and [2018, 1, 4, 0 - 2018, 1, 5, 0] 
-tc_8 = [
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=2, hour=0), "user_1"),
-    Interval(dt(year=2018, month=1, day=4, hour=0), dt(year=2018, month=1, day=5, hour=0), "user_1"),
-    Interval(dt(year=2018, month=1, day=1, hour=0), dt(year=2018, month=1, day=8, hour=0), "user_2"),
-]
-
-# input: many users with many intervals each
-# expected: many common intervals 
-tc_9 = [
-    Interval(dt(year=2018, month=1, day=1, hour=10), dt(year=2018, month=1, day=1, hour=12), "user_1"),
-    Interval(dt(year=2018, month=1, day=3, hour=10), dt(year=2018, month=1, day=3, hour=12), "user_1"),
-    Interval(dt(year=2018, month=1, day=1, hour=11), dt(year=2018, month=1, day=1, hour=13), "user_2"),
-    Interval(dt(year=2018, month=1, day=3, hour=11), dt(year=2018, month=1, day=3, hour=13), "user_2"),
-    Interval(dt(year=2018, month=1, day=1, hour=8), dt(year=2018, month=1, day=1, hour=18), "user_3"),
-    Interval(dt(year=2018, month=1, day=2, hour=8), dt(year=2018, month=1, day=2, hour=18), "user_3"),
-    Interval(dt(year=2019, month=1, day=1, hour=8), dt(year=2019, month=1, day=1, hour=18), "user_4"),
-    Interval(dt(year=2019, month=1, day=5, hour=0), dt(year=2019, month=1, day=6, hour=0), "user_4"),
-    Interval(dt(year=2019, month=1, day=5, hour=0), dt(year=2019, month=1, day=6, hour=0), "user_5"),
-    Interval(dt(year=2019, month=1, day=5, hour=0), dt(year=2019, month=1, day=6, hour=0), "user_5"),
-    Interval(dt(year=2019, month=1, day=5, hour=0), dt(year=2019, month=1, day=6, hour=0), "user_6")
-]        
 
 
 def find_all_common_intervals(interval_list):
@@ -97,12 +28,6 @@ def find_all_common_intervals(interval_list):
     interval_tree = IntervalTree(interval_list)
     
     for interval in interval_tree.items():
-        # compare interval against overlap_dict's existing found overlaps
-        # because overlap_dict may have new intervals not in interval_tree.
-        for overlap in overlap_dict.keys():
-            add_new_overlap_to_dict(overlap, interval, overlap_dict)
-            
-        # compare interval against other intervals in the tree
         other_intervals = find_other_intervals_which_overlap(interval_tree, interval)
         for other_interval in other_intervals:
             add_new_overlap_to_dict(interval, other_interval, overlap_dict)
@@ -110,47 +35,46 @@ def find_all_common_intervals(interval_list):
     return overlap_dict
 
 
-# Input: An IntervalTree and an Interval in the IntervalTree
-# Output: A set containing all other Intervals in the IntervalTree that intersect with the given
-#  Interval
 def find_other_intervals_which_overlap(tree, interval):
+    """
+    Find intervals in tree that intersect with interval.
+    :param tree: Interval Tree
+    :param interval: Interval Object
+    :return: set of Interval objects that intersect with interval
+    """
     tree.remove(interval)
     result = tree.search(interval.begin, interval.end) # returns a set
     return result
 
 
-# Input: Two Intervals and a dictionary (key: Interval; value: set of userids)
-# Output: None
-# Pre-condition: interval.data is a user id
 def add_new_overlap_to_dict(interval_a, interval_b, overlap_dict):
+    """
+    Modifies overlap_dict to contain the overlapping region between interval_a and
+    interval b.
+    :param interval_a: Interval
+    :param interval_b: Interval
+    :param overlap_dict: dict where key is Interval representing overlap, value is set of
+    data attributes associated with that overlap.
+    :return:
+    """
     overlap = find_overlap(interval_a, interval_b)
     if overlap is None:
         return
 
-    if overlap in overlap_dict:
+    if overlap in overlap_dict.keys():
         overlap_data = overlap_dict[overlap]
-        overlap_data |= {interval_a.data, interval_b.data}
+        overlap_data |= {interval_a.data, interval_b.data}  # update set
     else:
         overlap_dict[overlap] = {interval_a.data, interval_b.data}
 
 
-# Input: Two Intervals and a dictionary (key: Interval; value: set of userids)
-# Output: None
-# Pre-condition: interval.data is a user id
-def add_overlap_to_dict(overlap, current_interval, overlap_dict):
-    # this function seems redundant. TODO: investigate.
-    common = find_overlap(overlap, current_interval)
-    if common is None:
-        return
-    
-    if common in overlap_dict:
-        overlap_data = overlap_dict[overlap].copy()
-        overlap_data |= {current_interval.data} # update set of data
-        overlap_dict[common] = overlap_data # update key-value pair
-
-# Input: Two Intervals
-# Output: An Interval
 def find_overlap(interval_a, interval_b):
+    """
+    Find overlapping region between two intervals.
+    :param interval_a: Interval
+    :param interval_b: Interval
+    :return: Interval that represents the overlapping region.
+    """
     a_begin = interval_a.begin
     a_end = interval_a.end
     b_begin = interval_b.begin
@@ -174,7 +98,7 @@ def sortby_start(interval_keys):
     """
     Sort keys based on start of interval, in ascending order
     :param interval_keys: list of keys.
-    :return: sorted list.
+    :return: sorted keys list.
     """
     ref = []
     for interval in interval_keys:
@@ -339,43 +263,3 @@ def help_msg():
 def example_msg():
     return EXAMPLE_MSG
 
-
-def test_algo(tc, tprint=False):
-    """
-    prints output of find_all_common_intervals(tc) for inspection.
-
-    :param tc: list of Intervals of Datetimes.
-    :param tprint: boolean. Whether to format datetime into string.
-    """
-    overlap_dict = find_all_common_intervals(tc)
-    if tprint == True:
-        to_print = format_overlaps(overlap_dict)
-        print(to_print)
-    else:
-        for items in overlap_dict.items():
-            print(items)
-    print()
-
-
-def run_tests():
-    test_algo(tc_1)
-    test_algo(tc_2)
-    test_algo(tc_3)
-    test_algo(tc_4)
-    test_algo(tc_5, tprint=True)
-    test_algo(tc_6, tprint=True)
-    test_algo(tc_7, tprint=True)
-    test_algo(tc_8, tprint=True)
-    test_algo(tc_9, tprint=True)
-
-
-## Test datetime parser.
-# test_str = "mel , 02 feb, 1300 + 2h15m;"\
-#     + "jon, 02 feb , 1400+2h30m"
-#
-# results = parse_dt_string(test_str)
-# for i in results:
-#     print(i)
-
-## Test common time finder algorithm.
-# test_algo(results, tprint=True)
